e227beb91c439ff22d46515fe4979921f1707413
IRFAN2772 <124225869+IRFAN2772@users.noreply.github.com>
2024-03-21 16:44:03 +0530

    Add files via upload
    Signed-off-by: IRFAN2772 <124225869+IRFAN2772@users.noreply.github.com>

---
+++ utils.py
@@ -0,0 +1,14 @@
+from PIL import Image
+
+def compare_images(image1_path, image2_path):
+    image1 = Image.open(image1_path)
+    image2 = Image.open(image2_path)
+
+    encoding1 = face_recognition.face_encodings(np.array(image1))
+    encoding2 = face_recognition.face_encodings(np.array(image2))
+
+    if not encoding1 or not encoding2:
+        return False
+
+    match = face_recognition.compare_faces(encoding1, encoding2)
+    return any(match)
